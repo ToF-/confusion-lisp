6226ae0afaa733783c86541ea22eb00db71346b6
Christophe Thibaut <cthibaut@octo.com>
2023-02-18 12:43:08 +0100

    refactoring: effective result when term = result

--- confusion-solve.lisp
+++ confusion-solve.lisp
@@ -2,19 +2,21 @@
 (:export :solve))
 
 (defun solver (expression result power-of-ten)
-  (if (or (< expression result)
-          (= result 0) 
-          (and 
-            (< expression power-of-ten) 
-            (< result power-of-ten) 
-            (/= result expression))) nil
+  (if (= expression result) 
+    (list expression)
+    (if (or (< expression result)
+            (= result 0) 
+            (and 
+              (< expression power-of-ten) 
+              (< result power-of-ten) 
+              (/= result expression))) nil
       (let* ((left (floor expression power-of-ten))
              (right (rem expression power-of-ten))
              (target (- result right))
              (candidate (append (solver left target 10) (list right))))
         (if (= result (reduce '+ candidate))
           candidate
-          (solver expression result (* power-of-ten 10))))))
+          (solver expression result (* power-of-ten 10)))))))
 
 (defun solve (expression result)
   (solver expression result 10))
