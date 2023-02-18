a858daa1fe7a9f7d940944ac798c3910dbe55dff
Christophe Thibaut <cthibaut@octo.com>
2023-02-18 10:32:49 +0100

    renaming test

--- tests.lisp
+++ tests.lisp
@@ -7,9 +7,10 @@
     (assert-equal (list 2 3) (solve 23 5))
     (assert-equal (list 4 7) (solve 47 11)))
 
-(define-test three-terms-sum
+(define-test three-units-sum
     (assert-equal (list 2 3 4) (solve 234 9))
     (assert-equal (list 4 7 8) (solve 478 19)))
+
 
 (run-tests :all)
 (sb-ext:quit)
