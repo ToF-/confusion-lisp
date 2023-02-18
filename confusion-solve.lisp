789bc9fdc78784e3d592595a5e327a54a0a6d2ff
Christophe Thibaut <cthibaut@octo.com>
2023-02-18 12:52:31 +0100

    refactoring

--- confusion-solve.lisp
+++ confusion-solve.lisp
@@ -2,21 +2,15 @@
 (:export :solve))
 
 (defun solver (expression result power-of-ten)
-  (if (= expression result) 
-    (list expression)
-    (if (or (< expression result)
-            (= result 0) 
-            (and 
-              (< expression power-of-ten) 
-              (< result power-of-ten) 
-              (/= result expression))) nil
-      (let* ((left (floor expression power-of-ten))
-             (right (rem expression power-of-ten))
-             (target (- result right))
-             (candidate (append (solver left target 10) (list right))))
-        (if (= result (reduce '+ candidate))
-          candidate
-          (solver expression result (* power-of-ten 10)))))))
+  (cond ((= expression result) (list expression))
+        ((= 0 expression) nil)
+        ((and (< expression power-of-ten) (< result power-of-ten)) nil)
+        (t (let* ((left (floor expression power-of-ten))
+                  (right (rem expression power-of-ten))
+                  (target (- result right))
+                  (solved-left (solver left target 10)))
+             (if solved-left (append solved-left (list right))
+               (solver expression result (* power-of-ten 10)))))))
 
 (defun solve (expression result)
   (solver expression result 10))
